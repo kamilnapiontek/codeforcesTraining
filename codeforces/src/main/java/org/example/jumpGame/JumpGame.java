5b9a132e8a29e31cd87207b1899f01a13e05ceee
p1ontek <kknapiontek@gmail.com>
2023-04-21 21:34:34 +0200

    jump game

---
+++ codeforces/src/main/java/org/example/jumpGame/JumpGame.java
@@ -0,0 +1,69 @@
+package org.example.jumpGame;
+
+import java.util.List;
+
+public class JumpGame {
+    public int minimumJumps(List<Integer> listToJump) {
+
+        if (!isJumpPossible(listToJump)) {
+            throw new IllegalStateException("Jump is not possible.");
+        }
+
+        int currentIndex = 0;
+        int jumpsToReturn = 0;
+        int longestPossibleJump;
+        int placeInArray;
+        int difference = 0;
+
+        while (listToJump.get(currentIndex) + currentIndex < listToJump.size() - 1) {
+            longestPossibleJump = 0;
+            placeInArray = listToJump.get(currentIndex) - 1;
+            for (int i = 1; i <= listToJump.get(currentIndex); i++) {
+                if (longestPossibleJump < (listToJump.get(currentIndex + i) - placeInArray)) {
+                    longestPossibleJump = listToJump.get(currentIndex + i - placeInArray);
+                    difference = i;
+                }
+                placeInArray--;
+            }
+            currentIndex += difference;
+            jumpsToReturn++;
+        }
+        return jumpsToReturn + 1;
+    }
+
+    private boolean isJumpPossible(List<Integer> list) {
+        int howManyZerosInRow;
+        int possibleZeroIndex;
+        int extraSpace;
+        boolean possibleJump;
+        int currentIndex;
+        if (list.get(0) == 0) return false;
+
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) == 0) {
+                howManyZerosInRow = 1;
+                possibleZeroIndex = i + 1;
+                while (possibleZeroIndex < list.size() && list.get(possibleZeroIndex) == 0) {
+                    howManyZerosInRow++;
+                    possibleZeroIndex++;
+                    if (possibleZeroIndex == list.size() - 1) break;
+                }
+
+                extraSpace = 0;
+                possibleJump = false;
+                currentIndex = i - 1;
+                while (!possibleJump) {
+                    if (list.get(currentIndex) - extraSpace > howManyZerosInRow) possibleJump = true;
+                    else {
+                        currentIndex--;
+                        if (currentIndex == -1) return false;
+                        extraSpace++;
+                    }
+                }
+
+                i += howManyZerosInRow - 1;
+            }
+        }
+        return true;
+    }
+}
---
+++ codeforces/src/test/java/org/example/jumpGame/JumpGameTest.java
@@ -0,0 +1,46 @@
+package org.example.jumpGame;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class JumpGameTest {
+    private JumpGame jumpGame = new JumpGame();
+
+    @ParameterizedTest
+    @MethodSource("correctData")
+    void shouldReturnMinimumJumpsToPassTheTable(List<Integer> listToJump, int result) {
+        //when
+        int minimumJumps = jumpGame.minimumJumps(listToJump);
+        //then
+        Assertions.assertEquals(result, minimumJumps);
+
+    }
+
+    private static Stream<Arguments> correctData() {
+        return Stream.of(
+                Arguments.of(List.of(2, 3, 1, 1, 4), 2),
+                Arguments.of(List.of(2, 3, 0, 1, 4), 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("exceptionData")
+    void shouldContainExceptionWhenTheListCannotBeSkipped(List<Integer> listToJump) {
+        // when
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            jumpGame.minimumJumps(listToJump);
+        });
+    }
+
+    private static Stream<Arguments> exceptionData() {
+        return Stream.of(
+                Arguments.of(List.of(2, 3, 0, 0, 0, 0, 0, 5)),
+                Arguments.of(List.of(2, 1, 0, 5, 2))
+        );
+    }
+}
